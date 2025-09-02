b3d86dd22d7cfc0135be47e55797f972dae8e8b3
19-front <yankey.emma@yahoo.com>
2025-09-02 13:32:20 +0300

    tuple and set

---
+++ learn_python/tuple_set.py
@@ -0,0 +1,15 @@
+# to use tuple you must use circular bracket ()
+tup = (21,87,66,39,3,87,99)
+print(tup)
+print(tup[1])
+# tup[1] = 33
+
+# set
+s = {22,45,72,85,8,0,76,27}
+print(s)
+s = {76,89,46,92,87,98,8,76}
+print(s)
+s.add(66)
+print(s)
+s.remove(89)
+print(s)
