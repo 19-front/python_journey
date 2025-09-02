cebac036c2711cf7114ab9556f0f6782e3c4198f
19-front <yankey.emma@yahoo.com>
2025-09-02 12:09:19 +0300

    variables

---
+++ learn_python/variables.py
@@ -0,0 +1,31 @@
+# variables
+x = 2
+x + 2
+y = 3
+x + y
+x = 9
+
+# can use underscore to call previous operation
+
+x + 10
+print(x + y)
+name = "youtube"
+# "youtube"
+# [0123456]
+name + "rock"
+
+name[1] 
+print(name[6])
+
+name[-1]
+# it will start from the end
+name[-2]
+name[-7]
+print(name[0:2])
+print(name[1:4])
+print(name[2:])
+print(name[:4])
+print(name[3:10])
+# name[0:3] = "my" #does not support item assignment but there is a workaround
+print("my"+ name[3:])
+print(len(name))
