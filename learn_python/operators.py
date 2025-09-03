fc9110065e53a56e73fb7a546fec13174e10ec4a
19-front <yankey.emma@yahoo.com>
2025-09-03 19:50:34 +0300

    operators

---
+++ learn_python/operators.py
@@ -0,0 +1,41 @@
+# arithmetic
+x =2
+y =3
+y + x
+y - x
+y*x
+y/x
+
+# assignment
+x = x +2
+x += 2
+y *=3
+y -=3
+a,b = 5,6
+
+# unary operator
+n = 7
+-n = 7
+n = -n 
+
+# relational operators
+
+a < b
+a>b
+a == b
+b == a
+a=6
+a ==b
+a<=b
+a>=b
+a !=b
+
+# logical operators 'or, not, and'
+a = 5
+b = 4
+a < 8
+a < 8 and b < 5
+
+x = True
+not x
+x = not x
