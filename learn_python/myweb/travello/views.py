acc9f719f5dd34b027f294c59288fa6ee983dc2e
19-front <yankey.emma@yahoo.com>
2025-09-20 14:29:02 +0300

    using loop in index.html to fetch the data from django

--- learn_python/myweb/travello/views.py
+++ learn_python/myweb/travello/views.py
@@ -7,18 +7,23 @@ def index(request):
     dest1 = Destination()
     dest1.name = "Accra"
     dest1.desc = "The City that Never Sleep"
+    dest1.img = "destination_1.jpg"
     dest1.prices = 700
     
     #destination 2
     dest2 = Destination()
     dest2.name = "Takoradi"
     dest2.desc = "The City of Wonders"
+    dest2.img = "destination_2.jpg"
     dest2.prices = 610
     
     #destination 3
     dest3 = Destination()
     dest3.name = "Kumasi"
     dest3.desc = "The Garden City"
+    dest3.img = "destination_3.jpg"
     dest3.prices = 770
     
-    return render (request, "index.html", {"dest1": dest1, "dest2": dest2, "dest3": dest3})
+    dests = [dest1, dest2, dest3]
+    
+    return render (request, "index.html", {"dests": dests})
