0b486ebc8c723c12cb953cd2692ed9cbaba1b97c
19-front <yankey.emma@yahoo.com>
2025-09-20 13:52:08 +0300

    introduced dynamic variable from django into the index. destination class created in models.py def for index was created for dest1, dest2, dest3 index.html was updated with the variables

--- learn_python/myweb/travello/views.py
+++ learn_python/myweb/travello/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render (request, "index.html")
+    #destination 1
+    dest1 = Destination()
+    dest1.name = "Accra"
+    dest1.desc = "The City that Never Sleep"
+    dest1.prices = 700
+    
+    #destination 2
+    dest2 = Destination()
+    dest2.name = "Takoradi"
+    dest2.desc = "The City of Wonders"
+    dest2.prices = 610
+    
+    #destination 3
+    dest3 = Destination()
+    dest3.name = "Kumasi"
+    dest3.desc = "The Garden City"
+    dest3.prices = 770
+    
+    return render (request, "index.html", {"dest1": dest1, "dest2": dest2, "dest3": dest3})
